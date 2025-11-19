d15d24c173324ba9ba60ef8a68f12e219c3a1ba4
QADIROVV <sultanbekdzumagldiev@gmail.com>
2025-11-19 21:32:29 +0500

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+// const fs = require("fs");
+// fs.openSync("main.js", "w", "da");
+// 1 - masala
+
+// const myFunc = require("./main.js");
+// console.log(myFunc(2, 3));
+
+// 2 - masala
+
+// const fs  = require("fs");
+// fs.openSync("test.txt", "w");
+// fs.writeFileSync("test.txt", "i love karate and node.js")
+// const myText = fs.readFileSync("test.txt", "utf8");
+// console.log(myText);
+// fs.writeFileSync("salom.js", "//salom dunyo")
+// fs.writeFileSync("test.js", "const myFunc = require(\"./test.js\" \);\nconsole.log(myFunc(2, 3));");
+
+// 3 - masala
+
+// const fs = require("fs");
+// fs.writeFileSync("style.css", "champion")
+
+// import randomArr from "./masala.js";
+// import fs from "fs";
+// const mayArr = randomArr(10);
+// const myBuff = Buffer.from(JSON.stringify(mayArr));
+// fs.writeFileSync("salom.js", myBuff)
+// const log = fs.readFileSync("salom.js", "N1");
+// console.log(log);
+
+// import fs from "fs";
+// fs.openSync("home.js", "w");
+// fs.writeFileSync("home.js", "champion")
